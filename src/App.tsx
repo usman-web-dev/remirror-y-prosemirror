972ee5e300e9b1bf12ca08ffdf11391ae3bf6a74
Usman Developer <usman.developer002@gmail.com>
2024-06-20 03:01:19 +0500

    add remirror

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "remirror/styles/all.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+import { Remirror, ThemeProvider, useRemirror } from "@remirror/react";
+import { FC } from "react";
+import { YjsExtension } from "remirror/extensions";
+import { WebrtcProvider } from "y-webrtc";
+import * as Y from "yjs";
+
+const ydoc = new Y.Doc();
+const provider = new WebrtcProvider("remirror-yjs-demo", ydoc);
+
+const extensions = () => [new YjsExtension({ getProvider: () => provider })];
+
+export const App: FC = () => {
+  const { manager } = useRemirror({ extensions, core: { excludeExtensions: ["history"] } });
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-
-export default App
+    <ThemeProvider>
+      <Remirror manager={manager} autoFocus autoRender="end" />
+    </ThemeProvider>
+  );
+};
